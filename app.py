a1df78c52a45f55c73fad82fa324e430f288042b
Fiifi Amoah <fyfybaisieamoah6@gmail.com>
2022-06-16 01:12:25 +0000

    Added Imports

---
+++ app.py
@@ -0,0 +1,16 @@
+import os 
+from flask import Flask, render_template,request,redirect
+from flask import request, url_for, send_file, flash
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=2006)
