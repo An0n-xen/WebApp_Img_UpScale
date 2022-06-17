99853215692a5555bd98f901b65fbe5056e2f0a5
Fiifi Amoah <fyfybaisieamoah6@gmail.com>
2022-06-17 16:16:40 +0000

    Linked upload page with python

--- app.py
+++ app.py
@@ -5,11 +5,32 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(),'LR')
+app.secret_key = "Project Legacy"
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/upload', methods = ['GET','POST'])
+def uploadfiles():
+    if request.method == 'POST':
+        if 'Images' not in request.files:
+            return redirect(request.url)
 
+        files = request.files.getlist('Images')
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+
+    return "File Upload Nicely"
+    
 
 
 if __name__ == "__main__":
